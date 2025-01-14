e7b74cc797c558be255ebb7e0aaa5c3b99c0645c
Danuzio Alves Amaral - Ee <68485673+Engenharia-entendida@users.noreply.github.com>
2025-01-14 15:46:05 -0300

    EspMAC.ino

--- EspMAC.ino
+++ EspMAC.ino
@@ -4,7 +4,7 @@
 void setup() {
   Serial.begin(115200); // configurada a taxa de transferência em 115200 bits por segundo para a transmissão serial
   WiFi.mode(WIFI_MODE_STA); // configura o WIFi para o Modo de estação WiFi
-  
+  delay(200);
   Serial.print("Endereço MAC: ");
   Serial.println(WiFi.macAddress()); // retorna o endereço MAC do dispositivo
 
